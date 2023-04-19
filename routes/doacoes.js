76e09835051c41d33081f57749903faf1a4f0cfb
DGuilhoto <justme.dlg@gmail.com>
2023-04-19 14:24:45 -0300

    implementando rotas

---
+++ routes/doacoes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const doacoes = express.Router();
+
+doacoes.route('/')
+.get(async ( req, res) =>{
+    res.json({message: "Get funcionando"})
+})
+.post(async ( req, res) =>{
+    res.json({message: "post funcionando"})
+})
+.put(async ( req, res) =>{
+    res.json({message: "put funcionando"})
+})
+.delete(async ( req, res) =>{
+    res.json({message: "delete funcionando"})
+});
+
+module.exports = doacoes;
