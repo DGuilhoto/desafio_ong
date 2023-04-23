fed55b33abce1953752d400424864294692fc658
DGuilhoto <justme.dlg@gmail.com>
2023-04-23 10:05:34 -0300

    implementação da documentação swagger

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-const ong = require("./routes/ong")
-const doacoes = require('./routes/doacoes')
-//para receber a requisição no formato json
+
+
+const ong = require('./routes/ong');
+const doacoes = require('./routes/doacoes');
+
+// para receber a requisição no formato json
 app.use(express.json());
 
-//rotas
+// rotas
 app.use('/ong', ong);
 app.use('/ong/doacoes', doacoes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
-//define a rota para o servidor
-app.listen(3000);
+// define a porta para o servidor
+app.listen(process.env.DB_PORT);
